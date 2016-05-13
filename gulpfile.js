169a6d33aa78aaf46ebf2945943509255234809a
yobacca <olaf.ennen@gmail.com>
2016-05-14 01:06:37 +0200

    Release v0.8.3 - updated gulp tasks, fixed README.md

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,13 @@ const mocha = require('gulp-mocha');
 gulp.task('test', () =>
   gulp.src('./js/*.js')
     .pipe(istanbul({ includeUntested: true }))
+    .pipe(istanbul.hookRequire())
     .on('finish', () => {
-      gulp.src('./test/test.js', { read: false })
-        .pipe(mocha({ reporter: 'spec' }))
+      gulp.src('./test/*.js', { read: false })
+        .pipe(mocha())//{ reporter: 'spec' }))
         .pipe(istanbul.writeReports({
           dir: './coverage',
-          reporters: ['lcov'],
+          reporters: ['lcov', 'text-summary'],
           reportOpts: {
             dir: './coverage'
           }
